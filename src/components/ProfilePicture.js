f5a14d8d4db49dab9b40e36e76513d8391a1f7be
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 15:44:45 -0800

    ProfilePicture: basic structure

---
+++ src/components/ProfilePicture.js
@@ -0,0 +1,10 @@
+const ProfilePicture = () => {
+  return (
+    <img
+      src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1340/v1638741714/Thu_Website/profilepic.jpg'
+      alt='Profile picture'
+    ></img>
+  );
+};
+
+export default ProfilePicture;
