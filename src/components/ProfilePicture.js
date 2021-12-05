0a96b5a522e73638bc405e4db32420661a850923
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 15:57:56 -0800

    ProfilePicture: changed alt text

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -5,7 +5,7 @@ const ProfilePicture = () => {
     <img
       className='profile-picture'
       src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1195/v1638748280/Thu_Website/profilepic.jpg'
-      alt='Profile picture'
+      alt='Profile'
     ></img>
   );
 };
