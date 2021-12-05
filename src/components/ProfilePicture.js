65f026d550a24697fea37a52260dcaaba3b30662
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 15:53:52 -0800

    ProfilePicture styles

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,7 +1,10 @@
+import './ProfilePicture.css';
+
 const ProfilePicture = () => {
   return (
     <img
-      src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1340/v1638741714/Thu_Website/profilepic.jpg'
+      className='profile-picture'
+      src='https://res.cloudinary.com/buraiyen/image/upload/c_scale,w_1195/v1638748280/Thu_Website/profilepic.jpg'
       alt='Profile picture'
     ></img>
   );
