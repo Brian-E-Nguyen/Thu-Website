36898a995f987063f1235bf6a5411cbccdb34604
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 13:16:13 -0800

    import navbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Route } from 'react-router-dom';
 import Home from './views/Home';
 import Resume from './views/Resume';
 
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Route path=''>
+        <Navbar />
+        <Route path='/home'>
           <Home />
         </Route>
         <Route path='/resume'>
