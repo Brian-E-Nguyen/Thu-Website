c64c40d47813b41bf76491ae3d5ae59d04d97849
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-06 12:31:48 -0800

    About: styles and placeholder text

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,19 +1,33 @@
+import './About.css';
+
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
-      <p>
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit..
-      </p>
-      <p>
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit..
-      </p>
-      <p>
-        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit..
-      </p>
+      <h1 className='about__header'>About Me</h1>
+      <div className='about__description'>
+        <p>
+          I am an internation student who is currently studying at
+          <span className='about__description--emphasis'> Cypress College</span>
+          . I plan to transfer to
+          <span className='about__description--emphasis'>
+            {' '}
+            California State University, Fullerton{' '}
+          </span>
+          to obtain a
+          <span className='about__description--emphasis'>
+            {' '}
+            Bachelor's in Accounting{' '}
+          </span>
+        </p>
+        <p>
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit..
+        </p>
+        <p>
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit..
+        </p>
+      </div>
     </div>
   );
 };
