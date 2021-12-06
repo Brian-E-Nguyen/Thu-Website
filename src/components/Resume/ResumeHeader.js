f3a6fc8e87df536ce524eac52f2508a93b6c8edd
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 20:38:33 -0800

    Resume: fixed link positioning

--- src/components/Resume/ResumeHeader.js
+++ src/components/Resume/ResumeHeader.js
@@ -3,7 +3,7 @@ const ResumeHeader = () => {
     <div>
       <h1 className='resume__title'>Thu Nguyen</h1>
       {/* Resume Links */}
-      <p className='resume__link'>
+      {/* <p className='resume__links'>
         <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>
         <a
           className='resume__link'
@@ -12,7 +12,20 @@ const ResumeHeader = () => {
           https://www.linkedin.com/in/thuhng24/
         </a>
         <a href='#'>657-238-7860</a>
-      </p>
+      </p> */}
+      <div className='resume__links'>
+        <ul>
+          <li>
+            <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/thuhng24/'>
+              linkedin.com/in/thuhng24/
+            </a>
+          </li>
+          <li>657-238-7860</li>
+        </ul>
+      </div>
       <p>
         A California second-year business administration student seeking an
         internship position in accounting to apply knowledge and to support the
