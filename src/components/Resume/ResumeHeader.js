4f72c7dee64ebe60328d9a2f08d0d1897fffbbaf
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 19:51:16 -0800

    init ResumeHeader.js

---
+++ src/components/Resume/ResumeHeader.js
@@ -0,0 +1,25 @@
+const ResumeHeader = () => {
+  return (
+    <div>
+      <h1 className='resume__title'>Thu Nguyen</h1>
+      {/* Resume Links */}
+      <p className='resume__link'>
+        <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>
+        <a
+          className='resume__link'
+          href='https://www.linkedin.com/in/thuhng24/'
+        >
+          https://www.linkedin.com/in/thuhng24/
+        </a>
+        <a href='#'>657-238-7860</a>
+      </p>
+      <p>
+        A California second-year business administration student seeking an
+        internship position in accounting to apply knowledge and to support the
+        company’s growth.
+      </p>
+    </div>
+  );
+};
+
+export default ResumeHeader;
