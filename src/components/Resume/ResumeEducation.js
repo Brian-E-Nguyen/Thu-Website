08b811a8d50a6af3a06670432d2079327e49b7e1
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 19:04:39 -0800

    saving if I break anything

--- src/components/Resume/ResumeEducation.js
+++ src/components/Resume/ResumeEducation.js
@@ -7,19 +7,19 @@ const ResumeEducation = () => {
     <div>
       <h2 className='resume__header'>Education</h2>
       <Grid container spacing={2}>
-        <Grid item sm={12} md={6}>
+        <Grid item sm={12} md={4} lg={6}>
           <h3 className='resume__leftcolumn--header'>Cypress College</h3>
         </Grid>
-        <Grid item sm={12} md={6}>
+        <Grid item sm={12} md={8} lg={6}>
           <h3 className='resume__rightcolumn--header'>Jan. 2020 – Jan. 2023</h3>
         </Grid>
-        <Grid item sm={12} md={6}>
+        <Grid item sm={12} md={4} lg={6}>
           <p className='resume__leftcolumn--description'>
             Business Administration Associate in Science Degree for Transfer in
             Accounting
           </p>
         </Grid>
-        <Grid item sm={12} md={6}>
+        <Grid item sm={12} md={8} lg={6}>
           <p className='resume__rightcolumn--description'>
             GPA 4.0 Cypress, CA
           </p>
--- src/components/Resume/ResumeExperience.js
+++ src/components/Resume/ResumeExperience.js
@@ -6,14 +6,14 @@ const ResumeExperience = () => {
   return (
     <div>
       <h2 className='resume__header'>Work Experience</h2>
-      <Grid container spacing={2}>
-        <Grid item sm={12} md={10}>
+      <Grid container spacing={4}>
+        <Grid item sm={12} md={8}>
           <h3 className='resume__leftcolumn--header'>Jobee Tim Viec</h3>
         </Grid>
-        <Grid item sm={12} md={2}>
+        <Grid item sm={12} md={4}>
           <h3 className='resume__rightcolumn--header'>May 2020 – Aug. 2020</h3>
         </Grid>
-        <Grid item sm={12} md={10}>
+        <Grid item sm={12} md={8}>
           <p className='resume__leftcolumn--description'>Content Writer</p>
           <ul className='resume__list'>
             <li>
@@ -26,18 +26,18 @@ const ResumeExperience = () => {
             </li>
           </ul>
         </Grid>
-        <Grid item sm={12} md={2}>
+        <Grid item sm={12} md={4}>
           <p className='resume__rightcolumn--description'>Online</p>
         </Grid>
       </Grid>
       <Grid container spacing={2}>
-        <Grid item sm={12} md={10}>
+        <Grid item sm={12} md={8}>
           <h3 className='resume__leftcolumn--header'>Qynnatee’s Baby Studio</h3>
         </Grid>
-        <Grid item sm={12} md={2}>
+        <Grid item sm={12} md={4}>
           <h3 className='resume__rightcolumn--header'>May 2019 – Sep. 2019</h3>
         </Grid>
-        <Grid item sm={12} md={10}>
+        <Grid item sm={12} md={8}>
           <p className='resume__leftcolumn--description'>
             Photography Assistant
           </p>
@@ -52,7 +52,7 @@ const ResumeExperience = () => {
             </li>
           </ul>
         </Grid>
-        <Grid item sm={12} md={2}>
+        <Grid item sm={12} md={4}>
           <p className='resume__rightcolumn--description'>
             Ho Chi Minh City, Vietnam
           </p>
