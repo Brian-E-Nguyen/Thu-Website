2cdde0f95618dfdfb03b637c8e51eed305d9b9ba
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 20:18:01 -0800

    DownloadButton: basic structure

--- src/components/UI/DownloadButton.js
+++ src/components/UI/DownloadButton.js
@@ -1,3 +1,16 @@
-const DownloadButton = () => {};
+import Button from '@mui/material/Button';
+import { CloudDownload } from '@mui/icons-material';
+
+const DownloadButton = () => {
+  return (
+    <center>
+      <a href='/Thu_Resume.pdf' download>
+        <Button variant='contained' startIcon={<CloudDownload />}>
+          Download
+        </Button>
+      </a>
+    </center>
+  );
+};
 
 export default DownloadButton;
