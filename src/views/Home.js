0dff99cf20b4880661eb84db49e755df003e60cb
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 16:01:47 -0800

    Home: import About

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,11 @@
 import Introduction from '../components/Introduction';
+import About from '../components/About';
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <Introduction />
+      <About />
     </div>
   );
 };
