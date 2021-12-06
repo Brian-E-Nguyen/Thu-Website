14ba281d65f891711e677d079a1c95e25a959abe
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 16:09:57 -0800

    Home: import Contact

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,14 @@
 import Introduction from '../components/Introduction';
 import About from '../components/About';
+import Contact from '../components/Contact';
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <Introduction />
       <About />
+      <Contact />
     </div>
   );
 };
