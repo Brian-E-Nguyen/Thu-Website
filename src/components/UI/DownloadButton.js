adca5b3a8cacd1ef9d0061b800f29fb10917a12b
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 20:21:39 -0800

    DownloadButton styles

--- src/components/UI/DownloadButton.js
+++ src/components/UI/DownloadButton.js
@@ -1,10 +1,12 @@
 import Button from '@mui/material/Button';
 import { CloudDownload } from '@mui/icons-material';
 
+import './DownloadButton.css';
+
 const DownloadButton = () => {
   return (
     <center>
-      <a href='/Thu_Resume.pdf' download>
+      <a className='download-button' href='/Thu_Resume.pdf' download>
         <Button variant='contained' startIcon={<CloudDownload />}>
           Download
         </Button>
