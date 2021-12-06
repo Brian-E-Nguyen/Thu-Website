8c8e43dce7a0b37af01a308b930fcf73f1ce50dc
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-06 11:50:34 -0800

    renamed Home.js to HomeView.js

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,14 +2,16 @@ import Introduction from '../components/Introduction';
 import About from '../components/About';
 import Contact from '../components/Contact';
 
+import { Container } from '@mui/material';
+
 const Home = () => {
   return (
-    <div>
+    <Container>
       <h1>Home</h1>
       <Introduction />
       <About />
       <Contact />
-    </div>
+    </Container>
   );
 };
 
