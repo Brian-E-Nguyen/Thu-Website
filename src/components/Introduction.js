50a7049dad256648a08c48852ae33be64c23ae37
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-06 11:58:02 -0800

    Introduction styles

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,3 +1,5 @@
+import './Introduction.css';
+
 import { Grid } from '@mui/material';
 
 import ProfilePicture from './ProfilePicture';
@@ -5,11 +7,14 @@ import ProfilePicture from './ProfilePicture';
 const Introduction = () => {
   return (
     <Grid container spacing={2}>
-      <Grid item sm={12} md={6}>
-        <h1>Hi there!</h1>
-        <h1>My name is Thu Nguyen</h1>
+      <Grid item sm={12} md={8}>
+        <h1 className='introduction__header'>Hi there!</h1>
+        <h1 className='introduction__header'>
+          My name is <span className='introduction__name'>Thu Nguyen</span>
+        </h1>
+        <h2 className='introduction__subheader'> I'm an Aspiring Accountant</h2>
       </Grid>
-      <Grid item sm={12} md={6}>
+      <Grid item sm={12} md={4}>
         <ProfilePicture />
       </Grid>
     </Grid>
