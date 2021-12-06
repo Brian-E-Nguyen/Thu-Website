45ea6f7e91a02fdab3bb3d0030743d6dfa0acc96
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 16:01:27 -0800

    About: placeholder text

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,21 @@
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit..
+      </p>
+      <p>
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit..
+      </p>
+      <p>
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+        consectetur, adipisci velit..
+      </p>
+    </div>
+  );
 };
 
 export default About;
