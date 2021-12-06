f21ad7c5c7942b660b0bcde3c196ddff61031538
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 20:22:47 -0800

    ResumeView: import download button

--- src/views/ResumeView.js
+++ src/views/ResumeView.js
@@ -1,10 +1,13 @@
 import Resume from '../components/Resume/Resume';
+import DownloadButton from '../components/UI/DownloadButton';
 
 const ResumeView = () => {
   return (
     <div>
       <h1>Resume</h1>
+      <DownloadButton />
       <Resume />
+      <DownloadButton />
     </div>
   );
 };
