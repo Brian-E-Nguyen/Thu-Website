27a9198a0c267c5f91ef4beb4e7d54883c236cf4
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 16:42:32 -0800

    ResumeFile: Title and links

--- src/components/Resume/ResumeFile.js
+++ src/components/Resume/ResumeFile.js
@@ -1,3 +1,26 @@
-const ResumeFile = () => {};
+import { Container } from '@mui/material';
+
+import './ResumeFile.css';
+
+const ResumeFile = () => {
+  return (
+    <div className='resume'>
+      <Container>
+        <h1 className='resume__title'>Thu Nguyen</h1>
+        {/* Resume Links */}
+        <p className='resume__link'>
+          <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>
+          <a
+            className='resume__link'
+            href='https://www.linkedin.com/in/thuhng24/'
+          >
+            https://www.linkedin.com/in/thuhng24/
+          </a>
+          <a href='#'>657-238-7860</a>
+        </p>
+      </Container>
+    </div>
+  );
+};
 
 export default ResumeFile;
