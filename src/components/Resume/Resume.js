0a5241698c84609b364396611736c6201d431cd5
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 20:01:33 -0800

    Resume: import ResumeHeader

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,6 +4,7 @@ import ResumeEducation from './ResumeEducation';
 import ResumeExperience from './ResumeExperience';
 import ResumeVolunteer from './ResumeVolunteer';
 import ResumeSkills from './ResumeSkills';
+import ResumeHeader from './ResumeHeader';
 
 import './Resume.css';
 
@@ -11,23 +12,7 @@ const Resume = () => {
   return (
     <div className='resume'>
       <Container>
-        <h1 className='resume__title'>Thu Nguyen</h1>
-        {/* Resume Links */}
-        <p className='resume__link'>
-          <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>
-          <a
-            className='resume__link'
-            href='https://www.linkedin.com/in/thuhng24/'
-          >
-            https://www.linkedin.com/in/thuhng24/
-          </a>
-          <a href='#'>657-238-7860</a>
-        </p>
-        <p>
-          A California second-year business administration student seeking an
-          internship position in accounting to apply knowledge and to support
-          the company’s growth.
-        </p>
+        <ResumeHeader />
         <ResumeEducation />
         <ResumeExperience />
         <ResumeVolunteer />
