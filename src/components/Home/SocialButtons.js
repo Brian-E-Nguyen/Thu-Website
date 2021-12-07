cb1360a78565cb5c399c685c7cc18a975e62dcd8
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-07 13:59:20 -0800

    SocialButtons: imported all buttons

--- src/components/Home/SocialButtons.js
+++ src/components/Home/SocialButtons.js
@@ -1,3 +1,26 @@
-const SocialButtons = () => {};
+import IconButton from '@mui/material/IconButton';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const SocialButtons = () => {
+  return (
+    <div>
+      <IconButton color='secondary' component='span' size='large'>
+        <EmailIcon fontSize='large' />
+      </IconButton>
+      <IconButton color='secondary' component='span' size='large'>
+        <LinkedInIcon fontSize='large' />
+      </IconButton>
+      <IconButton color='secondary' component='span' size='large'>
+        <FacebookIcon fontSize='large' />
+      </IconButton>
+      <IconButton color='secondary' component='span' size='large'>
+        <InstagramIcon fontSize='large' />
+      </IconButton>
+    </div>
+  );
+};
 
 export default SocialButtons;
