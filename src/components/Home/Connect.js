e6f1af3d6aecac3cd079114bb4cf37d19aa6c917
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-07 13:02:43 -0800

    Connect: content and styles

--- src/components/Home/Connect.js
+++ src/components/Home/Connect.js
@@ -1,7 +1,13 @@
+import './Connect.css';
+
 const Connect = () => {
   return (
-    <div>
-      <h1>Connect With Me</h1>
+    <div className='connect'>
+      <h1 className='connect__header'>Connect</h1>
+      <p>
+        Feel free to use any of the platforms to contact / connect with me. I'd
+        love hearing from you!
+      </p>
       <button>Email</button>
       <button>LinkedIn</button>
       <button>Facebook</button>
