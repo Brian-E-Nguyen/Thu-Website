c46396956d6e9d656539270405543aa8c10fc075
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-07 14:05:16 -0800

    SocialButtons: working links

--- src/components/Home/SocialButtons.js
+++ src/components/Home/SocialButtons.js
@@ -5,20 +5,37 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
 const SocialButtons = () => {
+  const email = 'mailto:nhanhthu920@gmail.com';
+  const linkedIn = 'https://www.linkedin.com/in/thu-h-nguyen2/';
+  const facebook = 'https://www.facebook.com/profile.php?id=100045753340354';
+  const instagram = 'https://www.instagram.com/thu.n02/';
+
   return (
     <div>
-      <IconButton color='secondary' component='span' size='large'>
-        <EmailIcon fontSize='large' />
-      </IconButton>
-      <IconButton color='secondary' component='span' size='large'>
-        <LinkedInIcon fontSize='large' />
-      </IconButton>
-      <IconButton color='secondary' component='span' size='large'>
-        <FacebookIcon fontSize='large' />
-      </IconButton>
-      <IconButton color='secondary' component='span' size='large'>
-        <InstagramIcon fontSize='large' />
-      </IconButton>
+      {/* Email */}
+      <a href={email} target='_blank' rel='noreferrer'>
+        <IconButton color='secondary' component='span' size='large'>
+          <EmailIcon fontSize='large' />
+        </IconButton>
+      </a>
+      {/* LinkedIn */}
+      <a href={linkedIn} target='_blank' rel='noreferrer'>
+        <IconButton color='secondary' component='span' size='large'>
+          <LinkedInIcon fontSize='large' />
+        </IconButton>
+      </a>
+      {/* Facebook */}
+      <a href={facebook} target='_blank' rel='noreferrer'>
+        <IconButton color='secondary' component='span' size='large'>
+          <FacebookIcon fontSize='large' />
+        </IconButton>
+      </a>
+      {/* Instagram */}
+      <a href={instagram} target='_blank' rel='noreferrer'>
+        <IconButton color='secondary' component='span' size='large'>
+          <InstagramIcon fontSize='large' />
+        </IconButton>
+      </a>
     </div>
   );
 };
