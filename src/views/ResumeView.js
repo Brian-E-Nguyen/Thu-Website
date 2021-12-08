0fd96ca178d9638aee4f65e85edf3c2c9e9246dd
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-07 18:49:11 -0800

    import Particles

--- src/views/ResumeView.js
+++ src/views/ResumeView.js
@@ -1,9 +1,12 @@
 import Resume from '../components/Resume/Resume';
 import DownloadButton from '../components/UI/DownloadButton';
 
+import Particle from '../components/UI/Particle';
+
 const ResumeView = () => {
   return (
     <div>
+      <Particle />
       <h1>Resume</h1>
       <DownloadButton />
       <Resume />
