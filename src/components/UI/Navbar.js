a4c575b8440577ccef055db82b30127956f9976d
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-07 16:46:42 -0800

    Navbar: conditional for button display

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,15 +1,27 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const [mobileView, setMobileView] = useState(false);
+  const navbarBtn = (
+    <div className='navbar__toggle-btn'>
+      <span className='navbar__toggle-btn--bar'></span>
+      <span className='navbar__toggle-btn--bar'></span>
+      <span className='navbar__toggle-btn--bar'></span>
+    </div>
+  );
+  const showButton = () => {
+    if (window.innerWidth >= 732) setMobileView(false);
+    else setMobileView(true);
+  };
+
+  window.addEventListener('resize', showButton);
+  console.log(mobileView);
   return (
     <nav className='navbar'>
       <h1 className='navbar__brand'>Thu Nguyen</h1>
-      <a href='#' className='navbar__toggle-btn'>
-        <span className='navbar__toggle-btn--bar'></span>
-        <span className='navbar__toggle-btn--bar'></span>
-        <span className='navbar__toggle-btn--bar'></span>
-      </a>
+      {mobileView && navbarBtn}
       <div className='navbar__links'>
         <ul>
           <li>
