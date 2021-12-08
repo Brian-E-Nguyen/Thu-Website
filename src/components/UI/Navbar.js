4fd3fb2dfe8c1d62ced62ba25cc911f226ee6c34
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-07 16:02:26 -0800

    Navbar: initial styles

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,15 +1,22 @@
+import './Navbar.css';
+
 import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/resume'>Resume</Link>
-        </li>
-      </ul>
+    <nav className='navbar'>
+      <div>
+        <h1 className='navbar__brand'>Thu Nguyen</h1>
+      </div>
+      <div className='navbar__links'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className=''>
+            <Link to='/resume'>Resume</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
