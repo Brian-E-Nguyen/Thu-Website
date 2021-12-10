3433153bdb3cec6bffeda15b350afdabb218a498
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-10 11:17:57 -0800

    Footer: more styles

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -7,22 +7,25 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <Grid container spacing={3}>
-        <Grid item sm={12} md={4} className='footer__section'>
-          Designed and developed by{' '}
-          <a
-            href='https://www.brianenguyen.com/'
-            target='_blank'
-            className='footer__link'
-          >
-            Brian Nguyen
-          </a>
+        <Grid item xs={12} md={4} className='footer__section'>
+          <div>
+            Designed and developed by{' '}
+            <a
+              href='https://www.brianenguyen.com/'
+              target='_blank'
+              rel='noreferrer'
+              className='footer__link'
+            >
+              Brian Nguyen
+            </a>
+          </div>
         </Grid>
-        <Grid item sm={12} md={4} className='footer__section'>
+        <Grid item xs={12} md={4} className='footer__section'>
           <section className='footer__brand'>
             {new Date().getFullYear()} — Thu Nguyen
           </section>
         </Grid>
-        <Grid item sm={12} md={4} className='footer__section'>
+        <Grid item xs={12} md={4} className='footer__section'>
           <SocialButtonsSmall />
         </Grid>
       </Grid>
