011eb09fd9e1b9e32438b21226ec4d081027fa7e
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-10 11:58:23 -0800

    init ErrorView.js

---
+++ src/views/ErrorView.js
@@ -0,0 +1,10 @@
+const ErrorView = () => {
+  return (
+    <div>
+      <h1>404 Error</h1>
+      <h3>The page you are on doesn't or no longer exists</h3>
+    </div>
+  );
+};
+
+export default ErrorView;
