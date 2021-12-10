407a570c80d9dfd7a0ac994c8e59384ab122e168
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-10 12:15:56 -0800

    ErrorView: styles

--- src/views/ErrorView.js
+++ src/views/ErrorView.js
@@ -1,6 +1,8 @@
+import './ErrorView.css';
+
 const ErrorView = () => {
   return (
-    <div>
+    <div className='error'>
       <h1>404 Error</h1>
       <h3>The page you are on doesn't or no longer exists</h3>
     </div>
