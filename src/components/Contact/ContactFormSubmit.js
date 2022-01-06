f5f1a1eecff24c14b00201780640173ed23b8164
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-06 11:40:57 -0800

    temporary form submit logic

---
+++ src/components/Contact/ContactFormSubmit.js
@@ -0,0 +1,11 @@
+import emailjs from 'emailjs-com';
+
+export const submitForm = (event) => {
+  emailjs.sendForm(
+    'service_1xwndxg',
+    'template_fx6y7uj',
+    event.target,
+    'user_4vVpxSkvODPvEMWOc492H'
+  );
+  event.target.reset();
+};
