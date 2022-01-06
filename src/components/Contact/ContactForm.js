c417face7a49e3451f186797c3c7a3b6ae0c2f44
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-06 11:20:44 -0800

    ContactForm: basic structure

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,5 +1,49 @@
+import { Grid, TextField, Button } from '@mui/material';
+import './ContactForm.css';
 const ContactForm = () => {
-  return <form className='contact-form'></form>;
+  return (
+    <form className='contact-form'>
+      <Grid container spacing={5}>
+        {/* First name */}
+        <Grid item xs={6} md={6}>
+          <TextField
+            className='contact-form__field'
+            id='first-name'
+            label='First Name *'
+            name='first_name'
+          />
+        </Grid>
+        {/* Last name */}
+        <Grid item xs={6} md={6}>
+          <TextField
+            className='contact-form__field'
+            id='last-name'
+            label='Last Name *'
+            name='last_name'
+          />
+        </Grid>
+      </Grid>
+      {/* Email */}
+      <TextField
+        className='contact-form__field'
+        id='email'
+        label='Email *'
+        name='email'
+        type='email'
+      />
+
+      {/* Message */}
+      <TextField
+        className='contact-form__field'
+        multiline
+        maxRows={4}
+        id='message'
+        label='Your Message *'
+        name='message'
+      />
+      <Button>Submit</Button>
+    </form>
+  );
 };
 
 export default ContactForm;
