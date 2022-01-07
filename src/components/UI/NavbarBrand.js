967c91e53914186097b7f3b4477501c5a9c27f17
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-07 09:51:33 -0800

    increased size of navbar brand

--- src/components/UI/NavbarBrand.js
+++ src/components/UI/NavbarBrand.js
@@ -1,5 +1,5 @@
 const NavbarBrand = () => {
-  return <img src='./logo2.png' width='100px' alt='Thu Nguyen' />;
+  return <img src='./logo2.png' width='125px' alt='Thu Nguyen' />;
 };
 
 export default NavbarBrand;
