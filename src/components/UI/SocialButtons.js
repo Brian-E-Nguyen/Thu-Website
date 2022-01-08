1a5479e9bfbbd071a1f560c93898e4158c18c5c0
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-08 13:47:17 -0800

    removed facebook and instagram icons

--- src/components/UI/SocialButtons.js
+++ src/components/UI/SocialButtons.js
@@ -1,8 +1,6 @@
 import IconButton from '@mui/material/IconButton';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
 
 import './SocialButtons.css';
 
