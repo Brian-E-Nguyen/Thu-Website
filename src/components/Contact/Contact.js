f3be6ef381b1a604f94afb78b86a19bcb7239d95
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-08 13:20:09 -0800

    Contact: linkedin

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,15 @@ const Contact = () => {
       <h1>Contact Me</h1>
       <p>
         Feel free to contact me for any reason using the form below, or directly
-        at <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>
+        at <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>. You
+        can also{' '}
+        <a
+          href='https://www.linkedin.com/in/thu-h-nguyen2/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          connect with me on LinkedIn
+        </a>
       </p>
       <ContactForm />
     </div>
