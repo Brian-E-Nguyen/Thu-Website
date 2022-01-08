a787bfa8300d97fb0381d12f992ecd70f635af19
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-08 13:26:20 -0800

    fixed incoming changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,20 +1,23 @@
 import ContactForm from './ContactForm';
 const Contact = () => {
   return (
-    <div className='contact'>
-      <h1>Contact Me</h1>
-      <p>
-        Feel free to contact me for any reason using the form below, or directly
-        at <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>. You
-        can also{' '}
-        <a
-          href='https://www.linkedin.com/in/thu-h-nguyen2/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          connect with me on LinkedIn
-        </a>
-      </p>
+    <div id='contact'>
+      <h1 className='header'>Contact Me</h1>
+      <div className='description'>
+        <p>
+          Feel free to contact me for any reason using the form below, or
+          directly at{' '}
+          <a href='mailto:nhanhthu920@gmail.com'>nhanhthu920@gmail.com</a>. You
+          can also{' '}
+          <a
+            href='https://www.linkedin.com/in/thu-h-nguyen2/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            connect with me on LinkedIn
+          </a>
+        </p>
+      </div>
       <ContactForm />
     </div>
   );
